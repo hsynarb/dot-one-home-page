2c871eccee6b849e8fcd0d2b53dd71dcff426183
Hossein <en.h.arabi@gmail.com>
2025-08-29 17:25:04 +0330

    fix: fix title component display name

--- app/ui/title.tsx
+++ app/ui/title.tsx
@@ -4,29 +4,29 @@ import { forwardRef, RefObject } from "react";
 interface TitleProps {
   title: string;
   mainTitle?: string;
-  ref?: RefObject<HTMLDivElement>;
   className?: string;
 }
-const Title = forwardRef<HTMLDivElement, TitleProps>(
-  ({ title, mainTitle = "جمله کوتاه یا یک تایتل", className }, ref) => {
-    return (
-      <div ref={ref} className={classNames(className, "")}>
-        <div className="flex items-center gap-5">
-          <div>
-            <div className="flex text-xs gap-2">
-              <span>GROUP</span>
-              <span className="font-semibold">ONE</span>
-            </div>
-            <div className="flex items-center">
-              <div className="bg-[#939393] w-[140px] h-[1px]" />
-              <div className="bg-[#939393] w-[5px] h-[5px] rotate-45" />
-            </div>
+const Title = forwardRef<HTMLDivElement, TitleProps>(function Title(
+  { title, mainTitle = "جمله کوتاه یا یک تایتل", className },
+  ref
+) {
+  return (
+    <div ref={ref} className={classNames(className, "")}>
+      <div className="flex items-center gap-5">
+        <div>
+          <div className="flex text-xs gap-2">
+            <span>GROUP</span>
+            <span className="font-semibold">ONE</span>
           </div>
-          <span className="text-[#939393] mt-8">{title}</span>
+          <div className="flex items-center">
+            <div className="bg-[#939393] w-[140px] h-[1px]" />
+            <div className="bg-[#939393] w-[5px] h-[5px] rotate-45" />
+          </div>
         </div>
-        <p className="text-2.5xl mt-5">{mainTitle}</p>
+        <span className="text-[#939393] mt-8">{title}</span>
       </div>
-    );
-  }
-);
+      <p className="text-2.5xl mt-5">{mainTitle}</p>
+    </div>
+  );
+});
 export default Title;
